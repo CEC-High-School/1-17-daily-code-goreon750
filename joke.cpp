7bc30f571687e73bdc11667a43aa502753717b63
goreon750 <35533019+goreon750@users.noreply.github.com>
2018-01-17 08:22:41 -0700

    Add files via upload

---
+++ joke.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Tim, do you think i'm a good mother? " << endl << endl << endl << endl << endl;
+	cout << "my names Bob" << endl;
+
+}
